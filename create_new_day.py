0a787fdebc3ecea94e2a0e856ee441ea5d55ac39
Théo Lequesne <60904290+Jufevic@users.noreply.github.com>
2024-12-02 08:20:44 +0100

    Update day creation and Readme

--- create_new_day.py
+++ create_new_day.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from datetime import datetime
 import shutil
-from aocd import data
+from aocd import get_data
 
 day = datetime.now().day
 root_dir = Path(__file__).parent
@@ -10,8 +10,8 @@ day_dir = root_dir / f"day_{day:02}"
 if not os.path.exists(day_dir):
     os.makedirs(day_dir)
     with open(Path(day_dir / "input.txt"), "w") as f:
-        f.write(data)
+        f.write(get_data(day=day))
     Path(day_dir / "demo_input.txt").touch()
 
-    shutil.copyfile(root_dir / "template.py", day_dir / "part_1.txt")
-    Path(day_dir / "part_2.txt").touch()
+    shutil.copyfile(root_dir / "template.py", day_dir / "part_1.py")
+    Path(day_dir / "part_2.py").touch()
