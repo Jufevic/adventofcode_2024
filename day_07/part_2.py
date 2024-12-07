ec1ae28249508fcc46d6fe0f445c07d635565568
Théo Lequesne <60904290+Jufevic@users.noreply.github.com>
2024-12-07 09:43:35 +0100

    Solved day 7

---
+++ day_07/part_2.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+CURRENT_FOLDER = Path(__file__).absolute().parent
+INPUT_FILE = CURRENT_FOLDER / "input.txt"
+DEMO_INPUT_FILE = CURRENT_FOLDER / "demo_input.txt"
+
+
+total = 0
+with open(INPUT_FILE) as f:
+    for line in f.read().splitlines():
+        test_value, numbers = line.split(":")
+        test_value = int(test_value)
+        numbers = [int(i) for i in numbers.split()]
+        values = {numbers[0]}
+        for number in numbers[1:]:
+            new_values = set()
+            for value in values:
+                new_values.add(value + number)
+                new_values.add(value * number)
+                new_values.add(int(str(value) + str(number)))
+            values = new_values
+        if test_value in values:
+            total += test_value
+
+print(total)
